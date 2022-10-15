39924dc3b76863275ec06c3fc8f59807d0ee651a
Hamza Umar <humar6078@gmail.com>
2022-10-14 21:00:04 -0700

    Smile in the mirror done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lower alphabet in reverse
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char wds;
+
+	for (wrds = 'z'; wds >= 'a'; wds--)
+	{
+		putchar(wds);
+	}
+	putchar('\n');
+	return (0);
+}
